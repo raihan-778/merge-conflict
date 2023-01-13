5e2916849f881db8bcdc0ac4fc82340a6beacbf5
unknown <raihanarif778@gmail.com>
2023-01-13 23:27:57 +0600

    innitial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const index = () => {
+  return (
+    <div>
+      <h2>
+        One Item <i class="fa fa-address-card" aria-hidden="true"></i>
+      </h2>
+    </div>
+  );
+};
+
+export default index;
